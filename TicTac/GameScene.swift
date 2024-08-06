4f9673c63f6f93aa1c5630bc4f37f05f8f1954d6
AdrianKnudsen <adrian@knudsen.no>
2024-08-06 14:30:54 +0200

    Update GameScene.swift

--- TicTac/GameScene.swift
+++ TicTac/GameScene.swift
@@ -3,21 +3,27 @@ import SpriteKit
 class GameScene: SKScene {
 
     override func didMove(to view: SKView) {
+        setupSplashScreen()
+    }
+    
+    // Function to setup the splash screen
+    func setupSplashScreen() {
         let titleLabel = SKLabelNode(text: "Tic Tac Toe")
         titleLabel.fontSize = 65
         titleLabel.fontColor = .white
         titleLabel.position = CGPoint(x: frame.midX, y: frame.midY + 100)
         addChild(titleLabel)
 
-        let startButton = SKSpriteNode(color: .blue, size: CGSize(width: 200, height: 50))
+        let startButton = SKSpriteNode(color: .white, size: CGSize(width: 200, height: 50))
         startButton.name = "startButton"
         startButton.position = CGPoint(x: frame.midX, y: frame.midY)
         addChild(startButton)
 
         let startLabel = SKLabelNode(text: "Start Game")
         startLabel.fontSize = 25
-        startLabel.fontColor = .white
-        startLabel.position = CGPoint(x: 0, y: -10)
+        startLabel.fontColor = .black
+        startLabel.verticalAlignmentMode = .center
+        startLabel.horizontalAlignmentMode = .center
         startButton.addChild(startLabel)
     }
 
@@ -26,7 +32,7 @@ class GameScene: SKScene {
         let location = touch.location(in: self)
         let node = atPoint(location)
 
-        if node.name == "startButton" {
+        if node.name == "startButton" || node.parent?.name == "startButton" {
             let transition = SKTransition.flipHorizontal(withDuration: 0.5)
             let ticTacToeScene = TicTacToeScene(size: self.size)
             view?.presentScene(ticTacToeScene, transition: transition)
